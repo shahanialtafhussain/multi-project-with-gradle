d082b0372f2f23375e31468bd9efef9a482996c9
Calvin Pang <cal.pang123@gmail.com>
2019-04-30 15:04:34 +1000

    First commit

---
+++ account-service/src/main/groovy/com/multiproject/accountservice/AccountServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.multiproject.accountservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AccountServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication, args)
+	}
+
+}
---
+++ order-service/src/main/groovy/com/multiproject/orderservice/OrderServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.multiproject.orderservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OrderServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication, args)
+	}
+
+}
